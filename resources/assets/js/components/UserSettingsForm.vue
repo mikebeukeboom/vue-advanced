714c6878e630f02bd8cc7be0f791ad653d2a52b3
= <=>
2018-07-23 23:17:40 +0200

    adding first lesson material

---
+++ resources/assets/js/components/UserSettingsForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form class="card" @submit.prevent="submit">
+        <h1 class="text-2xl font-bold mb-6">Your Settings</h1>
+        <label class="block mb-6">
+            <span class="form-label mb-2">Email Address</span>
+            <input class="form-input" placeholder="you@example.com">
+        </label>
+        <div class="mb-8">
+            <span class="form-label mb-2">Newsletter</span>
+            <div class="flex justify-between item-center">
+                <p class="mr-4">Send you occasional news and updates</p>
+                <span class="toggle flex-no-shrink"role="checkbox" tabindex="0"></span>
+            </div>
+        </div>
+        <div class="text-right">
+            <button class="btn btn-blue">Update Settings</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+
+</style>
