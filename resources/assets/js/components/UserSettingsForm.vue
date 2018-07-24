27d2eac8fde858fb809a8e3c269d2ad0f1e16fd1
Mike <mike@flooris.nl>
2018-07-24 08:57:48 +0200

    Start

--- resources/assets/js/components/UserSettingsForm.vue
+++ resources/assets/js/components/UserSettingsForm.vue
@@ -3,13 +3,17 @@
         <h1 class="text-2xl font-bold mb-6">Your Settings</h1>
         <label class="block mb-6">
             <span class="form-label mb-2">Email Address</span>
-            <input class="form-input" placeholder="you@example.com">
+            <input class="form-input" v-model="email" placeholder="you@example.com">
         </label>
         <div class="mb-8">
             <span class="form-label mb-2">Newsletter</span>
             <div class="flex justify-between item-center">
                 <p class="mr-4">Send you occasional news and updates</p>
-                <span class="toggle flex-no-shrink"role="checkbox" tabindex="0"></span>
+                <span class="toggle flex-no-shrink"role="checkbox" tabindex="0"
+                      :aria-checked="receiveNewsletter.toString()"
+                      @click="toggleNewsletter"
+                      @keydown.space.prevent="toggleNewsletter"
+                ></span>
             </div>
         </div>
         <div class="text-right">
@@ -19,7 +23,25 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        data() {
+            return {
+                email: 'Jane@example.com',
+                receiveNewsletter: false,
+            }
+        },
+        methods: {
+            submit() {
+                console.log('Submitting preferences...', {
+                    email: this.email,
+                    receiveNewsletter: this.receiveNewsletter,
+                })
+            },
+            toggleNewsletter() {
+                this.receiveNewsletter = ! this.receiveNewsletter
+            }
+        }
+    }
 </script>
 
 <style scoped>
